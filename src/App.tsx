49fe4b1effa6c62178e01fae5f61ed724d106d05
Tofura2074 <tokaoka12@gmail.com>
2025-04-29 00:52:23 +0400

    add some styles

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,17 @@ function App() {
     <div className="w-screen h-screen flex flex-col  items-center bg-gray-100">
       <Header />
       <main className="w-full h-full flex items-center  gap-[5%]">
-        <aside className="min-w-[250px] w-[15%] h-1/2 bg-gray-200 shadow-lg flex flex-col items-center justify-center gap-[5%] shadow-xl rounded-tr-[50px] rounded-br-[50px]">
+        <aside className="absolute min-w-[250px] w-[15%] h-1/2 bg-gray-200 shadow-lg flex flex-col items-center justify-center gap-[5%] shadow-xl rounded-tr-[50px] rounded-br-[50px]">
           {SidebarDatas.map((data, index) => (
             <Fragment key={index}>
               <h1
                 onClick={() => setStateOfSidebar(index)}
                 style={{ height: `calc(100% / ${SidebarDatas.length})` }}
-                className={`text-2xl w-full  font-bold text-gray-800 font-serif cursor-pointer hover:bg-gray-300 flex items-center justify-center rounded-lg transition-all duration-300 ease-in-out ${
+                className={`text-2xl w-full ${
+                  index === 0
+                    ? "rounded-tr-[50px]"
+                    : index === SidebarDatas.length - 1 && "rounded-br-[50px]"
+                }  font-bold text-gray-800 font-serif cursor-pointer hover:bg-gray-300 flex items-center justify-center rounded-lg transition-all duration-300 ease-in-out ${
                   stateOfSidebar === index && "bg-gray-300"
                 }`}
               >
@@ -32,6 +36,7 @@ function App() {
             </Fragment>
           ))}
         </aside>
+        <div className="w-full p-[5%] h-full bg-black"></div>
       </main>
     </div>
   );
