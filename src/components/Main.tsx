fb20fc2ebec5a6faa5358c5d95a603619eba5dab
Tofura2074 <tokaoka12@gmail.com>
2025-04-30 22:00:13 +0400

    fix changes

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -44,7 +44,7 @@ const Main: React.FC<{
               ? "Education"
               : "Skills"}
           </h1>
-          <div className="w-full h-full text-lg text-[#163853] font-semibold overflow-y-auto max-h-[400px] ">
+          <div className="w-full h-full text-lg text-[#163853] font-semibold overflow-y-auto max-h-[500px] ">
             {stateOfSidebar === 0 && state ? (
               <TypingEffect
                 text="Hello, My Name is Giorgi Topuria I was born on December 27, 2000. I'm  Full-Stack Software Engineer.I am Self Educated Person and i am in that field almost 7 years already
@@ -60,55 +60,157 @@ const Main: React.FC<{
                 handleAddNextState={handleAddNextState}
               />
             ) : stateOfSidebar === 1 && state ? (
-              <TypingEffect
-                text={`I am a self-driven developer with extensive project experience, including building an e-commerce site for a clothing store (halted due to financial constraints) and a "Working Inspection" management system (canceled despite assurances of hiring). These projects showcase my ability to design and implement complex systems from scratch, such as secure authorization, Redis-based session management, role/task handling, user management, hierarchical workflows, interactive Georgian map integration, and action logging. I am eager to grow as a developer, learn new skills, and contribute meaningfully to a collaborative team.`}
-                Element={"p"}
-                className="indent-[2rem]"
-                handleAddNextState={handleAddNextState}
-              />
+              <>
+                {/* <TypingEffect
+                  text={`I am a self-driven developer with extensive project experience, including: I was building an e-commerce site for a clothing store (halted due to financial constraints) and a "Working Inspection" management system (canceled despite assurances of hiring). These projects showcase my ability to design and implement complex systems from scratch, such as secure authorization, Redis-based session management, role/task handling, user management, hierarchical workflows, interactive Georgian map integration, and action logging. I am eager to grow as a developer, learn new skills, and contribute meaningfully to a collaborative team.`}
+                  Element={"p"}
+                  className="indent-[2rem]"
+                  handleAddNextState={handleAddNextState}
+                /> */}
+                {typingEffectState >= 0 && (
+                  <>
+                    <TypingEffect
+                      text={`I am a self-driven developer with extensive project experience, including:`}
+                      Element={"p"}
+                      className="indent-[2rem]"
+                      handleAddNextState={handleAddNextStates}
+                    />
+                  </>
+                )}
+                {typingEffectState >= 1 && (
+                  <>
+                    <h2>Clothe E-Commerce</h2>
+                    <TypingEffect
+                      text={`I was building an e-commerce site for a clothing store (halted due to financial constraints)`}
+                      Element={"p"}
+                      className="indent-[2rem]"
+                      handleAddNextState={handleAddNextStates}
+                    />
+                  </>
+                )}
+                {typingEffectState >= 2 && (
+                  <>
+                    <h2>Goverment System</h2>
+                    <TypingEffect
+                      text={`"Working Inspection" management system (canceled despite assurances of hiring). These projects showcase my ability to design and implement complex systems from scratch, such as secure authorization, Redis-based session management, role/task handling, user management, hierarchical workflows, interactive Georgian map integration, and action logging. I am eager to grow as a developer, learn new skills, and contribute meaningfully to a collaborative team.`}
+                      Element={"p"}
+                      className="indent-[2rem]"
+                      handleAddNextState={handleAddNextStates}
+                    />
+                  </>
+                )}
+                
+              </>
             ) : stateOfSidebar === 2 && state ? (
-              <TypingEffect
-                text={`I studied at 181 Public School after that i  I enrolled in university and graduated a degree in Economics at Sokhumi State University. Additionally, I completed my studies at Gldani's College, specializing in IT Networking. I also studied at IT Step Academy for one year, focusing on cybersecurity, but had to leave due to high tuition costs. after that i go to the Military and I have done my duty to my country. in parallel almost 7 year i'm studying Software engineering using books, YouTube videos, and Udemy courses, I learned frontend languages (HTML, CSS, JavaScript). Subsequently, I expanded my knowledge by learning React.js, Redux, and frameworks such as Framer Motion (for animations). I then explored Next.js and later began studying Node.js, driven by a personal project to develop a Word generator application based on custom options that produced Word files. When presenting this project to the Working Inspection team, they asked if I could create a complete system. I explained my capabilities with my current stack, but they required ASP.NET Core for the backend. Motivated by this challenge, I started learning ASP.NET Core and acquired significant skills. Although the system I developed remains unfinished, it includes numerous functionalities and demonstrates my knowledge in that field.`}
-                Element={"p"}
-                className="indent-[2rem]"
-                handleAddNextState={handleAddNextState}
-              />
+              <>
+                {/* <TypingEffect
+                  text={`I studied at 181 Public School after that i  I enrolled in university and graduated a degree in Economics at Sokhumi State University. Additionally, I completed my studies at Gldani's College, specializing in IT Networking. I also studied at IT Step Academy for one year, focusing on cybersecurity, but had to leave due to high tuition costs. after that i go to the Military and I have done my duty to my country. in parallel almost 7 year i'm studying Software engineering using books, YouTube videos, and Udemy courses, I learned frontend languages (HTML, CSS, JavaScript). Subsequently, I expanded my knowledge by learning React.js, Redux, and frameworks such as Framer Motion (for animations). I then explored Next.js and later began studying Node.js, driven by a personal project to develop a Word generator application based on custom options that produced Word files. When presenting this project to the Working Inspection team, they asked if I could create a complete system. I explained my capabilities with my current stack, but they required ASP.NET Core for the backend. Motivated by this challenge, I started learning ASP.NET Core and acquired significant skills. Although the system I developed remains unfinished, it includes numerous functionalities and demonstrates my knowledge in that field.`}
+                  Element={"p"}
+                  className="indent-[2rem]"
+                  handleAddNextState={handleAddNextState}
+                /> */}
+                {typingEffectState >= 0 && (
+                  <>
+                    <h2>School</h2>
+                    <TypingEffect
+                      text={`I studied at 181 Public School, and i gradated it succesfully`}
+                      Element={"p"}
+                      className="indent-[2rem]"
+                      handleAddNextState={handleAddNextStates}
+                    />
+                  </>
+                )}
+                {typingEffectState >= 1 && (
+                  <>
+                    <h2>University</h2>
+                    <TypingEffect
+                      text={`after that in 2017 I enrolled in university and graduated a degree in "Economics" at "Sokhumi State University", in 2022`}
+                      Element={"p"}
+                      className="indent-[2rem]"
+                      handleAddNextState={handleAddNextStates}
+                    />
+                  </>
+                )}
+                {typingEffectState >= 2 && (
+                  <>
+                    <h2>College</h2>
+                    <TypingEffect
+                      text={`In Parallel in 2019 i Was studying at "Gldani's College" and graduated a degree in "IT Networking"`}
+                      Element={"p"}
+                      className="indent-[2rem]"
+                      handleAddNextState={handleAddNextStates}
+                    />
+                  </>
+                )}
+                {typingEffectState >= 3 && (
+                  <>
+                    <TypingEffect
+                      text={`In Parallel in 2021 i Was studying at "IT step academy" faculty of "Cybersecurity" for 1 year but i had to leave it because of high tuition fees`}
+                      Element={"p"}
+                      className="indent-[2rem]"
+                      handleAddNextState={handleAddNextStates}
+                    />
+                  </>
+                )}
+                {typingEffectState >= 4 && (
+                  <>
+                    <h2>Military</h2>
+                    <TypingEffect
+                      text={`In 2023 i go to the Military and I have done my duty to my country`}
+                      Element={"p"}
+                      className="indent-[2rem]"
+                      handleAddNextState={handleAddNextStates}
+                    />
+                  </>
+                )}
+                {typingEffectState >= 5 && (
+                  <>
+                    <h2>Self-Study Of Programming</h2>
+                    <TypingEffect
+                      text={`In parallel almost 7 year i'm studying Software engineering using books, YouTube videos, and Udemy courses, I learned frontend languages (HTML, CSS, JavaScript). Subsequently, I expanded my knowledge by learning React.js, Redux, and frameworks such as Framer Motion (for animations). I then explored Next.js and later began studying Node.js, driven by a personal project to develop a Word generator application based on custom options that produced Word files. When presenting this project to the Working Inspection team, they asked if I could create a complete system. I explained my capabilities with my current stack, but they required ASP.NET Core for the backend. Motivated by this challenge, I started learning ASP.NET Core and acquired significant skills. Although the system I developed remains unfinished, it includes numerous functionalities and demonstrates my knowledge in that field.`}
+                      Element={"p"}
+                      className="indent-[2rem]"
+                      handleAddNextState={handleAddNextStates}
+                    />
+                  </>
+                )}
+              </>
             ) : (
               stateOfSidebar === 3 &&
               state && (
                 <>
                   {typingEffectState >= 0 && (
                     <>
-                    <h2>Programming Skills</h2>
-                    <TypingEffect
-                      text={`In Software Engineering, I have a lot of knowledge which includes: React.js, Redux, Framer Motion, Next.js, Node.js, ASP.NET Core (C#), MongoDB, Express.js, HTML, CSS, JavaScript, Tailwind CSS, Bootstrap, TypeScript, GitHub, Postman, Figma, MySQL, Redis, and JWT.`}
-                      Element={"p"}
-                      className="indent-[2rem]"
-                      handleAddNextState={handleAddNextStates}
-                    />
+                      <h2>Programming Skills</h2>
+                      <TypingEffect
+                        text={`In Software Engineering, I have a lot of knowledge which includes: React.js, Redux, Framer Motion, Next.js, Node.js, ASP.NET Core (C#), MongoDB, Express.js, HTML, CSS, JavaScript, Tailwind CSS, Bootstrap, TypeScript, GitHub, Postman, Figma, MySQL, Redis, and JWT.`}
+                        Element={"p"}
+                        className="indent-[2rem]"
+                        handleAddNextState={handleAddNextStates}
+                      />
                     </>
                   )}
                   {typingEffectState >= 1 && (
                     <>
-                    <h2>Speaking Languages</h2>
+                      <h2>Speaking Languages</h2>
 
-                    <TypingEffect
-                      text={`As for Languages: English(B2), Russian(A2), Georgian(Native)`}
-                      Element={"p"}
-                      className="indent-[2rem]"
-                      handleAddNextState={handleAddNextStates}
+                      <TypingEffect
+                        text={`As for Languages: English(B2), Russian(A2), Georgian(Native)`}
+                        Element={"p"}
+                        className="indent-[2rem]"
+                        handleAddNextState={handleAddNextStates}
                       />
-                      </>
+                    </>
                   )}
                   {typingEffectState >= 2 && (
                     <>
-                    <h2>Other Skills</h2>
-                    <TypingEffect
-                      text={`Also i have Some knowledge in IT Field such as Networking, Cybersecurity, and Linux.`}
-                      Element={"p"}
-                      className="indent-[2rem]"
-                      handleAddNextState={handleAddNextStates}
-                    />
+                      <h2>Other Skills</h2>
+                      <TypingEffect
+                        text={`Also i have Some knowledge in IT Field such as Networking, Cybersecurity, and Linux.`}
+                        Element={"p"}
+                        className="indent-[2rem]"
+                        handleAddNextState={handleAddNextStates}
+                      />
                     </>
                   )}
                 </>
